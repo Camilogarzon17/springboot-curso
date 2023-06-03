a438be3ee24d9b672ad9883c32d6566610995c70
Brayant Camilo Garzon Larrota <cgarzon@Autogalias.local>
2023-06-03 12:00:00 -0500

    config datasource properties y clas commit

--- src/main/java/com/fundamentos/srpingboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/srpingboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentos.srpingboot.fundamentos.bean.MyBeanWhitPropertiesImplement
 import com.fundamentos.srpingboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @EnableConfigurationProperties(UserPojo.class)
 @Configuration
@@ -24,5 +27,14 @@ public class GeneralConfiguration {
     public MyBeanWhitProperties function(){
         return new MyBeanWhitPropertiesImplement(name, lastname);
 
+    }
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }

b2d3a584b5a92ece1e0293e0b87c9e5a6bb7b60e
Brayant Camilo Garzon Larrota <cgarzon@Autogalias.local>
2023-06-03 09:59:18 -0500

    logs commit

--- src/main/java/com/fundamentos/srpingboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/srpingboot/fundamentos/FundamentosApplication.java
@@ -3,6 +3,9 @@ package com.fundamentos.srpingboot.fundamentos;
 import com.fundamentos.srpingboot.fundamentos.bean.*;
 import com.fundamentos.srpingboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.srpingboot.fundamentos.component.ComponentTwoImplement;
+import com.fundamentos.srpingboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,15 +14,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {  //dependedncia inyectada
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWhitProperties myBeanWhitProperties;
-	public FundamentosApplication(@Qualifier("componentTwoImplement")ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWhitProperties myBeanWhitProperties){
+	private UserPojo userPojo;
+	public FundamentosApplication(@Qualifier("componentTwoImplement")ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWhitProperties myBeanWhitProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWhitProperties = myBeanWhitProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -31,5 +38,14 @@ public class FundamentosApplication implements CommandLineRunner {  //dependednc
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWhitProperties.function());;
+		System.out.println(userPojo.getEmail() +"-" + userPojo.getPassword() +"-"+ userPojo.getAge());
+		try{
+			//error
+			int value = 10 / 0;
+			LOGGER.debug("my value " + value);
+		}catch (Exception e){
+			LOGGER.error("This error aplication the division whit 0"+ e.getMessage());
+		}
+
 	}
 }
--- src/main/java/com/fundamentos/srpingboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/srpingboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentos.srpingboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements  MyBeanWithDependency{
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
      private MyOperation myOperation;
 
@@ -10,7 +15,9 @@ public class MyBeanWithDependencyImplement implements  MyBeanWithDependency{
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("we have entered the method printWithDependency ");
         int number = 1;
+        LOGGER.debug(" number sent as a parameter to the operation is: " + number);
         System.out.println("result: " + myOperation.sum(number));
         System.out.println("Hello world from the implementation of a good with dependency");
     }

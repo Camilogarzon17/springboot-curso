1aaa5b4fe0d67588f8be1de03e8a9f9b9cf13a82
Brayant Camilo Garzon Larrota <cgarzon@Autogalias.local>
2023-06-10 11:24:24 -0500

    uso Query methods como Or, And, OrderBy, Between, Sort

--- src/main/java/com/fundamentos/srpingboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/srpingboot/fundamentos/FundamentosApplication.java
@@ -53,26 +53,39 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private void getInformationJpqlFromUSer(){
 
-		LOGGER.info("Usuaior con el metodo findByUserName" + userRepository.findByName("Ashley").orElseThrow(() -> new RuntimeException("No se encontro el usuario")));
+	/*	LOGGER.info("Usuaior con el metodo findByUserName" + userRepository.findByName("Ashley").orElseThrow(() -> new RuntimeException("No se encontro el usuario")));
 
 		userRepository.findAndShort("", Sort.by("id").descending()).stream().forEach(user -> LOGGER.info( "usuario con metodo sort" + user));
 
 		userRepository.findByEmail("Brayant@hotmail.com").stream().forEach(user ->  LOGGER.info("usuario con Query metodo" + user));
 
-		LOGGER.info("usuario con Query metodo findByEmailAndName " + userRepository.findByEmailAndName("camilo@hotmail.com", "Camilo").orElseThrow(() -> new RuntimeException("usuraio no encontrado")));
+		LOGGER.info("usuario con Query metodo findByEmailAndName " + userRepository.findByEmailAndName("camilo@hotmail.com", "Camilo").orElseThrow(()
+				-> new RuntimeException("usuraio no encontrado")));
+
+		userRepository.findByNameLike("%n%").stream().forEach(user -> LOGGER.info("usuario encontrado con findByNameLike" + user));
+
+		userRepository.findByNameOrEmail("Ashley", null).stream().forEach(user -> LOGGER.info("usuario encontrado con findByNameOrEmail" + user)); */
+
+		userRepository.findByBirthDateBetween(LocalDate.of(2020,3, 19), LocalDate.of(2022, 12, 17))
+				.stream()
+				.forEach(user -> LOGGER.info("usuario con interbalo de fechas" + user));
+
+		/*userRepository.findByNameLikeOrderByIdDesc("%n%").stream().forEach(user -> LOGGER.info("usuario ordenado de manera descendente con findByNameLikeOrderByIdDesc" + user));*/
+
+		userRepository.findByNameContainingOrderByIdDesc("Camilo").stream().forEach(user -> LOGGER.info("usuario ordenado de manera descendente con findByNameLikeOrderByIdDesc" + user));
 	}
 
 	private void savesUserInDataBase(){
 		User user1 = new User("Camilo", "camilo@hotmail.com", LocalDate.of(2023, 12, 17));
-		User user2 = new User("Brayant", "Brayant@hotmail.com", LocalDate.of(2023, 11, 5));
+		User user2 = new User("Camilo", "Brayant@hotmail.com", LocalDate.of(2023, 11, 5));
 		User user3 = new User("Ashley", "Ashley@hotmail.com", LocalDate.of(2023, 10, 16));
 		User user4 = new User("Yulain", "Yulain@hotmail.com", LocalDate.of(2023, 9, 17));
 		User user5 = new User("Jorge", "Jorge@hotmail.com", LocalDate.of(2023, 8, 1));
 		User user6 = new User("Enrique", "Enrique@hotmail.com", LocalDate.of(2023, 7, 5));
 		User user7 = new User("Tatiana", "Tatiana@hotmail.com", LocalDate.of(2023, 6, 7));
 		User user8 = new User("Marlin", "Marlin@hotmail.com", LocalDate.of(2023, 5, 16));
-		User user9 = new User("Anderson", "Anderson@hotmail.com", LocalDate.of(2023, 4, 26));
-		User user10 = new User("Andres", "Andres@hotmail.com", LocalDate.of(2023, 3, 19));
+		User user9 = new User("Anderson", "Anderson@hotmail.com", LocalDate.of(2022, 4, 26));
+		User user10 = new User("Andres", "Andres@hotmail.com", LocalDate.of(2020, 3, 19));
 		List<User> list= Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10);
 		list.stream().forEach(userRepository::save);
 	}

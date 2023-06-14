6910455b6b457d5d59e371af9d03752daf6185a7
Brayant Camilo Garzon Larrota <cgarzon@Autogalias.local>
2023-06-14 12:59:43 -0500

    Pagination con springboot

--- src/main/java/com/fundamentos/srpingboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentos/srpingboot/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.fundamentos.srpingboot.fundamentos.caseuse.DeleteUser;
 import com.fundamentos.srpingboot.fundamentos.caseuse.GetUser;
 import com.fundamentos.srpingboot.fundamentos.caseuse.UpdateUser;
 import com.fundamentos.srpingboot.fundamentos.entity.User;
+import com.fundamentos.srpingboot.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +21,14 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser, DeleteUser deleteUser, UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -47,5 +51,9 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id) {
         return new ResponseEntity<>(UpdateUser.update(newUser, id), HttpStatus.OK);
+    }
+    @GetMapping("/pegeable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 }
